c7153bae3546828f779eb8f9b6621a5b1679d7e1
ADILAYOUB <mintofree840@gmail.com>
2023-05-10 23:11:41 +0530

    interface

---
+++ lib/06 oops/14interface.dart
@@ -0,0 +1,23 @@
+//? Introduction
+// In this section, you will learn the dart interface and how to implement an
+// interface with the help of examples.
+// In Dart, every class is implicit interface. Before learning about the
+// interface in dart, you should have a basic understanding of the class
+// and objects, inheritance and abstract class in Dart.
+
+// Interface In Dart
+// An interface defines a syntax that a class must follow.
+// It is a contract that defines the capabilities of a class.
+// It is used to achieve abstraction in the Dart programming language.
+// When we implement an interface, you must implement all the properties
+// and methods defined in the interface. Keyword implements is used to
+// implement an interface.
+
+// Syntax Of Interface In Dart
+// class InterfaceName {
+// code
+// }
+
+// class ClassName implements InterfaceName {
+//   code
+// }
