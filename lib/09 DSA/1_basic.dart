3a1449b52a317c8cba440a409661368daa23da08
ADILAYOUB <mintofree840@gmail.com>
2023-05-20 12:36:55 +0530

    commit details

--- lib/09 DSA/1_basic.dart
+++ lib/09 DSA/1_basic.dart
@@ -4,25 +4,27 @@
 // memory location
 // Dart provides the build in support to array through ""LIST"" class
 
- List<int> numbers = [1, 2, 3, 4, 5];
-print(numbers[0]); // Output: 1
+void shahid() {
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print(numbers[0]); // Output: 1
 
-List<String> name = ['Shahid', 'Gulam', 'Faried', 'Aftaq'];
+  List<String> name = ['Shahid', 'Gulam', 'Faried', 'Aftaq'];
 
-print(name[0]);// Output: Shahid
+  print(name[0]);
+} // Output: Shahid
 //Print based on index
 
-//! 2 Linked List 
-// a linked list is a data structure consists of a sequence of nodes 
-// where each node contains data and a ref. to the nex node 
-// in dart we can use Linked List by using class and refrence 
+//! 2 Linked List
+// a linked list is a data structure consists of a sequence of nodes
+// where each node contains data and a ref. to the nex node
+// in dart we can use Linked List by using class and refrence
 //? Example of linked list and traversing its elements
 
 class Node {
   int data = 0;
   Node? next;
-    
-    Node({
-     this.next ,
+
+  Node({
+    this.next,
   });
 }
