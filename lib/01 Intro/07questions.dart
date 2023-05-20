64e55f6e37b86902a685eb0ca5a4aacd031e44ad
ADILAYOUB <mintofree840@gmail.com>
2023-05-20 12:35:29 +0530

    basic changes

--- lib/01 Intro/07questions.dart
+++ lib/01 Intro/07questions.dart
@@ -3,7 +3,8 @@
  * /// !QUESTIONS FOR PRACTICE 1
 Basic Dart Practice Questions
 Write a program to print your name in Dart.
-Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+Write a program to print Hello I am “Adil Ayoub” and Hello I’am “Adil Ayoub” with
+ single and double quotes.
 Declare constant type** of int set value 7.
 Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
 Write a program to print a square of a number using user input.
