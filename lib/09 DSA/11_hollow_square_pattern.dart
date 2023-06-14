49293a043d7a5c7545aa45b413703b7f0e074ce9
ADILAYOUB <mintofree840@gmail.com>
2023-06-14 23:52:59 +0530

    hollow pattern

---
+++ lib/09 DSA/11_hollow_square_pattern.dart
@@ -0,0 +1,21 @@
+// in sold square we have grid of * matrics
+
+// now we want to print only the boundary of the matrix;
+
+void main() {
+  hollowSquare(8);
+}
+
+void hollowSquare(int number) {
+  for (int i = 0; i < number; i++) {
+    String line = '';
+    for (int j = 0; j < number; j++) {
+      if (i == 0 || i == number - 1 || j == 0 || j == number - 1) {
+        line += '*';
+      } else {
+        line += ' ';
+      }
+    }
+    print(line);
+  }
+}
